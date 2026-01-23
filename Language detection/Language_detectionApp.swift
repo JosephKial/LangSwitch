d0846ba3dc28cb33d085627f467b501e9e9441aa
my macbook <y.kiali8523@gmail.com>
2026-01-23 13:28:41 +0200

    Initial Commit

---
+++ Language detection/Language_detectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Language_detectionApp.swift
+//  Language detection
+//
+//  Created by yosef kiali on 23/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct Language_detectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
